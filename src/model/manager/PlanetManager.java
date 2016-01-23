7e36f89af2f126db86645bdd32d080cc53d3919a
ii-BLACKREAPER <brandonrexbrown@email.arizona.edu>
2016-01-23 16:44:34 -0700

    created a galaxy
    enums and stars and shit

--- src/model/manager/PlanetManager.java
+++ src/model/manager/PlanetManager.java
@@ -1,15 +1,21 @@
 package model.manager;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.EnumSet;
 
+import gameLogic.Atmosphere;
+import gameLogic.Elements2000K;
 import gameLogic.GameState;
 import gameLogic.Planet;
 import gameLogic.PlanetFactory;
+import gameLogic.Star;
+import gameLogic.StarFactory;
 
 public class PlanetManager extends Manager {
 
 	private PlanetFactory planetFactory;
-	
+	private StarFactory starFactory = new StarFactory();
 	/*
 	 * PlanetManager()
 	 * 
@@ -26,7 +32,22 @@ public class PlanetManager extends Manager {
 		// TODO Auto-generated method stub
 	}
 	
-	public void addPlanet() {
+	public void addPlanet(){
 		//myGame.addPlanet(planetFactory.newPlanet(INFO))
+	}
+	
+	public void createGalaxy() {
+		ArrayList<StarNames> galaxy = new ArrayList<StarNames>(EnumSet.allOf(StarNames.class));
+		Collections.shuffle(galaxy);
+		int rand;
+		double random;
+		for(StarNames names : galaxy){
+			random = Math.random();;
+			rand = (int)(random * 101);
+			if(rand < 10){
+				rand = 10;
+			}
+			starFactory.theStar(rand, names.toString());
+		}
 	}
 }
