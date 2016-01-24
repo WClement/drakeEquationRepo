3764a90cfe4c8a39019a722efc6040b099b358ee
gbasadre <gbasadre@email.arizona.edu>
2016-01-23 20:24:52 -0700

    Buuuuuurp

--- src/gameLogic/Mothership.java
+++ src/gameLogic/Mothership.java
@@ -2,25 +2,29 @@ package gameLogic;
 
 import java.util.ArrayList;
 
-public abstract class Player {
-
+public abstract class Mothership {
+	
 	private ResourceArrayWrapper resources = new ResourceArrayWrapper();
 	private ArrayList<Planet> colonies = new ArrayList<Planet>();
-	
-	public ResourceArrayWrapper getResources() {
-		return resources;
-	}
-	
-	public void setResources(ResourceArrayWrapper resources) {
-		this.resources = resources;
-	}
-	
+	private int baseHealth;
+	private int currentHealth;
+	private int numDocks;
+	private int numDockedShips;
+
 	public ArrayList<Planet> getColonies() {
 		return colonies;
 	}
-	
+
 	public void setColonies(ArrayList<Planet> colonies) {
 		this.colonies = colonies;
 	}
-	
+
+	public ResourceArrayWrapper getResources() {
+		return resources;
+	}
+
+	public void setResources(ResourceArrayWrapper resources) {
+		this.resources = resources;
+	}
+
 }
